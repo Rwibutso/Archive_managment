ba1c8fa570307176d31f43d0e6b3a0b0ef9b0552
Hubert Kanyamahanga <kanyamahanga.hubert@aims-senegal.org>
2022-03-02 08:08:31 +0000

    Updayed

--- documents/models.py
+++ documents/models.py
@@ -5,8 +5,6 @@ class Files(models.Model):
     name = models.CharField(max_length=30)
     owner = models.CharField(max_length=30)
 
-    dkhdaf = models.
-
     def __str__(self):
         return self.name
 
